014a701ac35aaf46eb6ca51dd949942162f86dc5
Torrenava <51089658+Torrenava@users.noreply.github.com>
2024-12-28 19:06:55 +0100

    Create InstallPlugins.sh
    Añadido Script para instalar los plugins de ZSH.

---
+++ Linux/InstallPlugins.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo -e " [+] Instalando Plugins"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
+
+
+echo -e "\n [+] Plugins ZSH Instalados"
