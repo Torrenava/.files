df770a25fd60dadce5792a975167f014f575b2c4
Torrenava <torrenava@gmail.com>
2025-01-10 19:23:28 +0100

    Added Windows PowerShell Profile

---
+++ .windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,44 @@
+#########
+# NOTES #
+#########
+
+# Unblock-File ***.ps1
+# Permite ejecutar un archivo PowerShell sin importar la restriccion.
+
+#
+# PERSONAL SETTINGS
+#
+
+# OhMyPosh
+oh-my-posh init pwsh --config C:\Users\$Env:UserName\AppData\Local\Programs\oh-my-posh\themes\wopian.omp.json | Invoke-Expression
+
+
+
+# WeTransfer (Transferwee)
+function wetransfer($action, $file) {
+
+    $scriptPath = "C:\Users\$Env:UserName\Documents\Programas\transferwee\transferwee.py"
+    python $scriptPath $action $file
+}
+
+# New Installation
+function TInstall() {
+    sudo winget install eza-community.eza GnuWin32.Grep JanDeDobbeleer.OhMyPosh Git.Git Microsoft.PowerShell GNU.nano schollz.croc sharkdp.bat --silent
+}
+
+# Comandos ls
+Remove-Alias ls
+function ls() {
+    eza --icons @Args
+}
+
+function ll() {
+    eza --icons -l @Args
+}
+
+function la(){
+    eza --icons -a @Args
+}
+
+# Add Grep Path
+$env:Path += ";C:\Program Files (x86)\GnuWin32\bin"
