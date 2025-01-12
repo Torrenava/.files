c0e2cf074c00be73a01daf5daebcbdf7e7741c72
Torrenava <torrenava@gmail.com>
2025-01-12 20:47:44 +0100

    Added Flameshot Install + Removed i3 Window Title

--- Scripts/main.sh
+++ Scripts/main.sh
@@ -13,7 +13,7 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install gpg bat curl wget fzf zsh eza xstow micro gping traceroute net-tools vivid zoxide -y
+sudo apt install gpg bat curl wget fzf zsh eza xstow micro gping traceroute net-tools vivid zoxide flameshot -y
 sudo snap install dog -y
 
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
