efff2dbc8ca8d19912096fef6796c0a7a9ca5fe1
Torrenava <torrenava@gmail.com>
2025-01-12 17:11:08 +0100

    Permit Change Brightness without root

--- Scripts/InstallI3.sh
+++ Scripts/InstallI3.sh
@@ -4,4 +4,7 @@
 sudo apt update
 sudo apt install i3 picom polybar feh brightnesscctl -y
 
+# Permite cambiar el brillo siendo USER
+sudo usermod -aG video $USER
+
 echo -e " [I] Completed. Run 'cd ~/.files; stow .' to link dotfiles."
