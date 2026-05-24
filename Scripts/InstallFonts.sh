fd63030a7e3ac4de0d0303cbdb919c70fc82a415
Torrenava <torrenava@gmail.com>
2026-05-24 19:27:10 +0200

    Removed old scripts

--- Scripts/InstallFonts.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts
-wget "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaCode.zip"
-unzip CascadiaCode.zip
-rm CascadiaCode.zip
-fc-cache -fv
